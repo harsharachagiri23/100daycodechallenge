4c76ce4955ae152f68d630f3d1390d861cbfce7f
harsharachagiri23 <harsharachagiri23@gmail.com>
2024-01-03 17:34:57 -0500

    Pattern 2

--- Patterns/src/Patterns2.java
+++ Patterns/src/Patterns2.java
@@ -1,2 +1,19 @@
 public class Patterns2 {
+    static void Pattern2(int n){
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j <= i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args){
+        int n =3;
+        Pattern2(n);
+    }
 }
